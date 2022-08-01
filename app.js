c18d10eec1db1e9a22ff8ae0a4a3d08d94e20f86
Nemanja <manojlovic911@hotmail.com>
2022-08-01 22:39:36 +0200

    Add Redirect and route conditional blocks + Node libuv async

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 // Imports
 const http = require("http");
+const fs = require("fs");
 
 // func for event
 function reqListener(req, res) {
-  // Logs the req received
-  console.log(req.url, req.method, req.headers);
+  let url = req.url;
+  let method = req.method;
+
+  if (url === "/") {
+    // Constructs the resp package that is going to be sent
+    // over the http protocol
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><br/><button type="submit">Submit</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    // Package Buffer
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    // On package stream end
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+
+      // Write message to file
+      fs.writeFile("message.txt", message, () => {
+        // Response status code
+        res.statusCode = 302;
+        // Redirect header
+        res.setHeader("Location", "/");
+
+        return res.end();
+      });
+    });
+  }
 
   // Constructs the resp package that is going to be sent
   // over the http protocol
@@ -21,4 +58,4 @@ function reqListener(req, res) {
 // incoming request
 const server = http.createServer(reqListener);
 
-server.listen("3000", "localhost");
+server.listen("8000", "localhost");
