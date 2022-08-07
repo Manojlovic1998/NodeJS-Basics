f1132ad21e473e83ac778eddb74b7bcdf9c68f03
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-07 17:21:56 +0200

    Init express app

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-// Imports
+// Core NodeJS Imports
 const http = require("http");
+
+// Third Party Package Imports
+const express = require("express");
+
+// Instantiate Express App
+const app = express();
 
 // Takes event listener as an arg.
 // Simply a function that will execute for every
 // incoming request
-const server = http.createServer(routes);
+const server = http.createServer(app);
 
 server.listen("3000", "localhost");
