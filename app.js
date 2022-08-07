1d9f225d6a9240cac00322ba2c50a55251574acd
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-07 17:26:58 +0200

    Add middleware examples to express app

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const express = require("express");
 // Instantiate Express App
 const app = express();
 
+// Middleware
+app.use((req, res, next) => {
+  console.log("First middleware!");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Second middleware!");
+  res.send("<h1>This is second middleware response!</h1>");
+});
+
 // Takes event listener as an arg.
 // Simply a function that will execute for every
 // incoming request
