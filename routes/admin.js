d4aef15debc6ee3649d42d782fa049ea76657980
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-09 21:19:30 +0200

    Export 'products' array and 'router' in different key value pairs

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,15 @@ const path = require("path");
 
 // Third Party Package Imports
 const express = require("express");
+
+// Project Imports
 const rootDir = require("../util/path");
 
 // Create Express.js router
 const router = express.Router();
+
+// Dummy Products Data
+const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
@@ -16,7 +21,9 @@ router.get("/add-product", (req, res, next) => {
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
