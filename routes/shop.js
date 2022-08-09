a15fed3f1762399bc899b9d2cd227ca2c10fe076
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-09 21:21:52 +0200

    Log the products array data

--- routes/shop.js
+++ routes/shop.js
@@ -3,13 +3,17 @@ const path = require("path");
 
 // Third Party Package Imports
 const express = require("express");
+// Project Imports
 const rootDir = require("../util/path");
+const adminData = require("./admin");
+
 // Express.js Router
 const router = express.Router();
 
 // Middleware
 router.get("/", (req, res, next) => {
   // __dirname global variable that points to the folder in which we are using it
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
