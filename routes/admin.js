ca2d6b58c292083040971e86281e29a2a14ee2fa
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-10 23:06:33 +0200

    Render add-product pug

--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,6 @@ const path = require("path");
 // Third Party Package Imports
 const express = require("express");
 
-// Project Imports
-const rootDir = require("../util/path");
-
 // Create Express.js router
 const router = express.Router();
 
@@ -15,14 +12,13 @@ const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { docTitle: "Add New Product" });
 });
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
   products.push({ title: req.body.title });
-  res.redirect("/");
+  res.render("shop", { products, docTitle: "Shop" });
 });
 
 exports.routes = router;
