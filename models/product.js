37401f2f9d862bb5fa592455b8da07f37662babf
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-13 22:08:01 +0200

    Factorise product model methods using helper function

--- models/product.js
+++ models/product.js
@@ -4,22 +4,31 @@ const path = require("path");
 // Project's imports
 const rootPath = require("../util/path");
 
+// Path where data should be stored
+const filePath = path.join(rootPath, "data", "products.json");
+
+// Util for getting products from file
+const getProductsFromFile = (renderCallback) => {
+  // Try to read the file at the filePath
+  fs.readFile(filePath, (error, fileData) => {
+    // If error return empty list
+    if (error) {
+      renderCallback([]);
+    }
+
+    // Fallback, if no error, parse json and return it
+    renderCallback(JSON.parse(fileData));
+  });
+};
+
 class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const filePath = path.join(rootPath, "data", "products.json");
-
-    fs.readFile(filePath, (error, fileData) => {
-      let products = [];
-
-      if (!error) {
-        products = JSON.parse(fileData);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
-
       fs.writeFile(filePath, JSON.stringify(products), (error) => {
         console.log(error);
       });
@@ -27,13 +36,7 @@ class Product {
   }
 
   static fetchAll(templateRenderCallback) {
-    const filePath = path.join(rootPath, "data", "products.json");
-    fs.readFile(filePath, (error, fileData) => {
-      if (error) {
-        templateRenderCallback([]);
-      }
-      templateRenderCallback(JSON.parse(fileData));
-    });
+    getProductsFromFile(templateRenderCallback);
   }
 }
 
