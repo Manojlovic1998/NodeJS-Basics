5d1625a48524d353e75b59e091af46d8d747168a
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-13 21:35:37 +0200

    Update save method for product model to save data to file

--- models/product.js
+++ models/product.js
@@ -1,4 +1,8 @@
-const products = [];
+// Node.js core module imports
+const fs = require("fs");
+const path = require("path");
+// Project's imports
+const rootPath = require("../util/path");
 
 class Product {
   constructor(title) {
@@ -6,7 +10,20 @@ class Product {
   }
 
   save() {
-    products.push(this);
+    const filePath = path.join(rootPath, "data", "products.json");
+
+    fs.readFile(filePath, (error, fileData) => {
+      let products = [];
+
+      if (!error) {
+        products = JSON.parse(fileData);
+      }
+      products.push(this);
+
+      fs.writeFile(filePath, JSON.stringify(products), (error) => {
+        console.log(error);
+      });
+    });
   }
 
   static fetchAll() {
