93555e67f4568f884d2b00983e121ebb1ee445d1
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-14 07:00:36 +0200

    Improve controllers dir structure and add base controlers for new routes

---
+++ controllers/admin.js
@@ -0,0 +1,40 @@
+const Product = require("../models/product");
+
+const getProductList = (req, res, next) => {
+  res.render("admin/product-list", {
+    docTitle: "Admin Product Listing",
+    path: "/admin/product-list",
+  });
+};
+
+const getEditProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
+    docTitle: "Edit Product",
+    path: "/admin/edit-product",
+  });
+};
+
+const getAddProduct = (req, res, next) => {
+  res.render("admin/add-product", {
+    docTitle: "Add New Product",
+    path: "/admin/add-product",
+  });
+};
+
+const postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+
+  const product = new Product(title, description, imageUrl, price);
+  product.save();
+  res.redirect("/");
+};
+
+module.exports = {
+  getAddProduct,
+  postAddProduct,
+  getProductList,
+  getEditProduct,
+};
---
+++ controllers/shop.js
@@ -0,0 +1,31 @@
+const Product = require("../models/product");
+
+const getProducts = (req, res, next) => {
+  // __dirname global variable that points to the folder in which we are using it
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      products,
+      docTitle: "Shop",
+      path: "/products",
+    });
+  });
+};
+
+const getIndex = (req, res, next) => {
+  res.render("shop/index", { docTitle: "Landing Page", path: "/" });
+};
+
+const getCart = (req, res, next) => {
+  res.render("shop/cart", { docTitle: "Cart", path: "/cart" });
+};
+
+const getCheckout = () => {
+  res.render("shop/checkout", { docTitle: "Checkout", path: "/checkout" });
+};
+
+module.exports = {
+  getProducts,
+  getIndex,
+  getCart,
+  getCheckout,
+};
